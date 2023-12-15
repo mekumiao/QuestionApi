3011f17fcdd54fa65b67f7c482162ad91633b517
mekumiao <btguigushi@qq.com>
2023-12-15 14:58:56 +0800

    修改用户角色时添加学生信息

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -87,7 +87,17 @@ public class UsersController(ILogger<UsersController> logger, QuestionDbContext 
         using var transaction = await _dbContext.Database.BeginTransactionAsync();
         if (dto.Roles is not null) {
             await _dbContext.UserRoles.Where(v => v.UserId == userId).ExecuteDeleteAsync();
-            await userManager.AddToRolesAsync(item, dto.Roles);
+            await userManager.AddToRolesAsync(item, dto.Roles.Distinct());
+
+            if (dto.Roles.Contains("student")) {
+                if (!await _dbContext.Students.AnyAsync(v => v.UserId == userId)) {
+                    _dbContext.Students.Add(new Student {
+                        UserId = userId,
+                        Name = item.UserName ?? string.Empty,
+                    });
+                    await _dbContext.SaveChangesAsync();
+                }
+            }
         }
         _mapper.Map(dto, item);
         await userManager.UpdateAsync(item);
