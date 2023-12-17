fac538d3b4a56ed2bbe7524b620e2ef9f2091e04
mekumiao <btguigushi@qq.com>
2023-12-18 07:13:59 +0800

    修改AnswerText字段规则

--- Database/Models/Student.cs
+++ Database/Models/Student.cs
@@ -30,7 +30,7 @@ public class StudentAnswer {
     public AnswerHistory AnswerHistory { get; set; } = null!;
     /// <summary>
     /// 答案:
-    /// 1.单选题和多选题，用英文逗号","隔开）
+    /// 1.单选题和多选题，多选题直接将答案拼接即可。如：ABC
     /// 2.判断题，0表示错，1表示对
     /// 3.填空题，直接填入文本
     /// </summary>
