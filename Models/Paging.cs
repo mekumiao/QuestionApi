b7ec111c7670e8e20cd96744d2222bf4c11b2509
mekumiao <btguigushi@qq.com>
2023-12-23 17:56:05 +0800

    分页模型limit最小值设为0

--- Models/Paging.cs
+++ Models/Paging.cs
@@ -5,7 +5,7 @@ namespace QuestionApi.Models;
 public class Paging {
     [Range(minimum: 0, maximum: int.MaxValue)]
     public int Offset { get; set; }
-    [Range(minimum: 10, maximum: 100)]
+    [Range(minimum: 0, maximum: 100)]
     public int Limit { get; set; } = 10;
 
     public IQueryable<T> Build<T>(IQueryable<T> queryable) where T : class {
