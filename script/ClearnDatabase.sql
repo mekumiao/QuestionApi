1eba3349f916963d84fbf96b5735ba8882155f9a
mekumiao <btguigushi@qq.com>
2023-12-25 15:03:50 +0800

    添加清空数据库数据脚本

---
+++ script/ClearnDatabase.sql
@@ -0,0 +1,8 @@
+DELETE FROM "AnswerHistories";
+DELETE FROM "StudentAnswers";
+DELETE FROM "Examinations";
+DELETE FROM "ExamPapers";
+DELETE FROM "Options";
+DELETE FROM "Questions";
+DELETE FROM "Students";
+DELETE FROM "AspNetUsers" WHERE "Id" != 1;
